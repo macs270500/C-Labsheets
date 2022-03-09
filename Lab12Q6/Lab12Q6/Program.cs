aa4a180e7b54f53545c3bc0042ddc855ba278c82
macs270500 <81253179+macs270500@users.noreply.github.com>
2022-03-09 13:49:09 +0400

    Add files via upload

---
+++ Lab12Q6/Lab12Q6/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System;
+
+namespace Lab12Q6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of hours worked: ");
+            double HrsWorked = double.Parse(Console.ReadLine());
+            Console.Write("Enter pay rate: ");
+            double PayRate = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Gross pay is : {CalcGrossPay(HrsWorked, PayRate)}");
+            Console.ReadKey();
+        }
+
+        static double CalcGrossPay(double HrsWrkd, double payRate)
+        {
+            double grosspay;
+            if (HrsWrkd <= 40)
+            {
+                grosspay = HrsWrkd * payRate;
+            }
+            else
+            {
+                grosspay = (40 * payRate) + ((HrsWrkd - 40) * (1.5 * payRate));
+            }
+            return grosspay;
+        }
+    }
+}
---
+++ Lab12Q7/Lab12Q7/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab12Q7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double HrsWrkd = double.Parse(txtHrsWrkd.Text);
+            double payrate = double.Parse(txtPayRate.Text);
+            double GrossPay = CalcGrossPay(HrsWrkd, payrate);
+            txtGrossPay.Text = GrossPay.ToString();
+        }
+        static double CalcGrossPay(double HrsWrkd, double payRate)
+        {
+            double grosspay;
+            if (HrsWrkd <= 40)
+            {
+                grosspay = HrsWrkd * payRate;
+            }
+            else
+            {
+                grosspay = (40 * payRate) + ((HrsWrkd - 40) * (1.5 * payRate));
+            }
+            return grosspay;
+        }
+    }
+}
