ed739a20c8636fb4ac15070aefca83666df2e70e
macs270500 <81253179+macs270500@users.noreply.github.com>
2022-03-09 14:24:07 +0400

    Add files via upload

---
+++ Lab19Q3/Lab19Q3/frmEMS.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Lab19Q3
+{
+    partial class frmEMS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtHrlyRate = new System.Windows.Forms.TextBox();
+            this.txtHrsWrkd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lstEmployee = new System.Windows.Forms.ListBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(263, 57);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(263, 83);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(263, 135);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(120, 59);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(116, 20);
+            this.txtID.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(120, 85);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(116, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtHrlyRate
+            // 
+            this.txtHrlyRate.Location = new System.Drawing.Point(120, 111);
+            this.txtHrlyRate.Name = "txtHrlyRate";
+            this.txtHrlyRate.Size = new System.Drawing.Size(116, 20);
+            this.txtHrlyRate.TabIndex = 2;
+            // 
+            // txtHrsWrkd
+            // 
+            this.txtHrsWrkd.Location = new System.Drawing.Point(120, 137);
+            this.txtHrsWrkd.Name = "txtHrsWrkd";
+            this.txtHrsWrkd.Size = new System.Drawing.Size(116, 20);
+            this.txtHrsWrkd.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Employee Management System";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Id:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Hourly Rate:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Hours Worked:";
+            // 
+            // lstEmployee
+            // 
+            this.lstEmployee.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstEmployee.FormattingEnabled = true;
+            this.lstEmployee.ItemHeight = 14;
+            this.lstEmployee.Location = new System.Drawing.Point(26, 182);
+            this.lstEmployee.Name = "lstEmployee";
+            this.lstEmployee.Size = new System.Drawing.Size(312, 158);
+            this.lstEmployee.TabIndex = 13;
+            this.lstEmployee.SelectedIndexChanged += new System.EventHandler(this.lstEmployee_SelectedIndexChanged);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(263, 109);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // frmEMS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 358);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.lstEmployee);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHrsWrkd);
+            this.Controls.Add(this.txtHrlyRate);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "frmEMS";
+            this.Text = "EMS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtHrlyRate;
+        private System.Windows.Forms.TextBox txtHrsWrkd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox lstEmployee;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
+
+
---
+++ Lab19Q3/Lab19Q3/frmEMS.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab19Q3
+{
+    public partial class frmEMS : Form
+    {
+        public frmEMS()
+        {
+            InitializeComponent();
+            lstEmployee.Items.Add(String.Format(fmtStr, "Id", "Employee Name", "Grosspay"));
+        }
+
+        List<clsEmployee> allEmployee = new List<clsEmployee>();        
+        string fmtStr = "{0,-10}{1,-20}{2,-10}";        
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            clsEmployee Emp = new clsEmployee();
+            Emp.EmpId = int.Parse(txtID.Text);
+            Emp.EmpName = txtName.Text;
+            Emp.PayRate = double.Parse(txtHrlyRate.Text);
+            Emp.HrsWrkd = double.Parse(txtHrsWrkd.Text);
+            allEmployee.Add(Emp);
+            lstEmployee.Items.Add(String.Format(fmtStr, Emp.EmpId, Emp.EmpName, Emp.GrossPay()));
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (lstEmployee.SelectedIndex>0) 
+            {
+                int index = lstEmployee.SelectedIndex;
+                clsEmployee Emp = new clsEmployee();
+                Emp.EmpId = int.Parse(txtID.Text);
+                Emp.EmpName = txtName.Text;
+                Emp.PayRate = double.Parse(txtHrlyRate.Text);
+                Emp.HrsWrkd = double.Parse(txtHrsWrkd.Text);
+                allEmployee.RemoveAt(lstEmployee.SelectedIndex - 1);
+                allEmployee.Insert(index - 1, Emp);
+                lstEmployee.Items.RemoveAt(lstEmployee.SelectedIndex);
+                lstEmployee.Items.Insert(index, String.Format(fmtStr, Emp.EmpId, Emp.EmpName, Emp.GrossPay()));
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstEmployee.SelectedIndex>0)
+            {
+                allEmployee.RemoveAt(lstEmployee.SelectedIndex - 1);
+                lstEmployee.Items.RemoveAt(lstEmployee.SelectedIndex);
+                btnClear_Click(sender, e);
+            } 
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtID.Clear();
+            txtName.Clear();
+            txtHrlyRate.Clear();
+            txtHrsWrkd.Clear();            
+        }
+
+        private void lstEmployee_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            clsEmployee Emp = new clsEmployee();
+            if (lstEmployee.SelectedIndex > 0)
+            {
+                Emp = allEmployee[lstEmployee.SelectedIndex - 1];
+                txtID.Text = Emp.EmpId.ToString();
+                txtName.Text = Emp.EmpName;
+                txtHrlyRate.Text = Emp.PayRate.ToString();
+                txtHrsWrkd.Text = Emp.HrsWrkd.ToString();
+            }
+        }
+    }
+}
