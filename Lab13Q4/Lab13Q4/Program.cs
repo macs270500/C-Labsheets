e25058dd173b49fdfba73a97858298e4a067bce6
macs270500 <81253179+macs270500@users.noreply.github.com>
2022-03-09 13:53:50 +0400

    Add files via upload

---
+++ Lab13Q4/Lab13Q4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab13Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the radius of the sphere: ");
+            double radius = Double.Parse(Console.ReadLine());
+            double volume = SphereVolume(radius);
+            Console.WriteLine($"The volume of the sphere is {volume.ToString("N2")}");
+            Console.ReadKey();
+        }
+        static double SphereVolume(double radius)
+        {
+            return (4.0 / 3.0) * Math.PI * Math.Pow(radius, 3);
+        }
+    }
+}
